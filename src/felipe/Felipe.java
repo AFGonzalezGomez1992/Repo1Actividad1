8732735646ca8d3dde031ff7e57d6330bda913c7
Andres Felipe <andresfgonzalezgomez@gmail.com>
2024-05-06 02:59:04 +0200

    user2 ha implantado el paquete felipe

---
+++ src/felipe/Felipe.java
@@ -0,0 +1,13 @@
+package felipe;
+
+public class Felipe {
+public void imprimirNombre() {
+    	
+        System.out.println("Hola, soy Felipe");
+    }
+public static void main(String[] args) {
+	Felipe felipe = new Felipe();
+	felipe.imprimirNombre();
+}
+
+}
