479f715ca3108cd0912d006e81048761e9cb6eaa
Andres Felipe <andresfgonzalezgomez@gmail.com>
2024-05-06 02:22:51 +0200

    manager ha creado el proyecto base

---
+++ src/developer/Manager.java
@@ -0,0 +1,19 @@
+package developer;
+
+public class Manager {
+    public void imprimirNombre() {
+    	
+        System.out.println("Hola, soy el Manager");
+    }
+
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.imprimirNombre();
+    }
+}
+
+
+	
+
+	
+
