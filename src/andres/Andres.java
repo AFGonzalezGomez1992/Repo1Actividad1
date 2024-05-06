1537723a9c321b956931cf732d38484deca240ee
Andres Felipe <andresfgonzalezgomez@gmail.com>
2024-05-06 02:42:35 +0200

    user1 ha implantado el paquete andres

---
+++ src/andres/Andres.java
@@ -0,0 +1,13 @@
+package andres;
+
+public class Andres {
+public void imprimirNombre() {
+    	
+        System.out.println("Hola, soy Andres");
+    }
+public static void main(String[] args) {
+	Andres andres = new Andres();
+	andres.imprimirNombre();
+}
+
+}
